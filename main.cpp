5f49cd90c05f7916a2d8107376ddd674e6118356
jesulayomy <jesulayomi081@gmail.com>
2022-09-21 22:08:39 +0100

    Uploaded the countdown cpp folder

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int countup =11;
+
+    int countdn;
+
+    for (countdn = 29;countdn >=1; countdn-- ) {
+
+            cout << countdn<< "--> Layo is best"<<endl;
+
+    }
+
+    for (int countup=1;countup <=17; countup++ ) {
+
+        cout <<countup<< "-> Yes" <<endl;
+    }
+
+    cout <<countup<<endl;
+
+    return 0;
+}
